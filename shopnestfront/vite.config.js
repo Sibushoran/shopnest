5b5242ffd7909f19b14d4c5a518460f9ea8b5ffa
sibushoran <rubensibu@gmail.com>
2025-06-06 15:46:15 +0530

    wrererf

--- shopnestfront/vite.config.js
+++ shopnestfront/vite.config.js
@@ -3,5 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ This ensures relative paths work in production (especially on Vercel)
 })
